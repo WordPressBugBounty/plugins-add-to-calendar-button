fae0e1cc726424ef6819e33164806e92bb5e2ba3
Sean Murphy <sean@iamseanmurphy.com>
2026-04-17 20:25:23 +0000

    2.9.5

--- add-to-calendar-button/build/block.asset.php
+++ add-to-calendar-button/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '8f2d5e229446fc7ee371');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '7775a3ff3124aaa0d925');
